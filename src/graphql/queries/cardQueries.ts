c2d4203bd5835122ecdbddf880b0e207fe824833
Morten Bendeke <mbendeke@hotmail.com>
2024-12-15 12:25:15 +0100

    fixed format search

--- src/graphql/queries/cardQueries.ts
+++ src/graphql/queries/cardQueries.ts
@@ -5,27 +5,31 @@ const cardQueries = {
   cards: async (_: any, { params }: { params: any }) => {
     const { limit, skip, lang, color, name, type, legalities, setId } = params || {};
 
-    // Sanitize user input
     const sanitizedParams = {
-      limit: sanitize(limit),
-      skip: sanitize(skip),
-      lang: sanitize(lang),
-      color: sanitize(color),
-      name: sanitize(name),
-      type: sanitize(type),
-      legalities: sanitize(legalities),
-      setId: sanitize(setId),
+      limit: sanitize(limit?.toString() || ''),
+      skip: sanitize(skip?.toString() || ''),
+      lang: sanitize(lang || ''),
+      color: sanitize(color || ''),
+      name: sanitize(name || ''),
+      type: sanitize(type || ''),
+      setId: sanitize(setId || ''),
     };
+
+    const sanitizedLegalities: any = {};
+    if (legalities && typeof legalities === 'object') {
+      Object.keys(legalities).forEach(key => {
+        sanitizedLegalities[key] = sanitize(legalities[key]);
+      });
+    }
 
     const filter: any = {};
     if (sanitizedParams.lang) filter.lang = sanitizedParams.lang;
     if (sanitizedParams.color) filter.color_identity = { $in: sanitizedParams.color.split(',') };
     if (sanitizedParams.name) filter.name = new RegExp(sanitizedParams.name, 'i'); // Case-insensitive search
     if (sanitizedParams.type) filter.type_line = new RegExp(sanitizedParams.type, 'i'); // Case-insensitive search
-    if (sanitizedParams.legalities) {
-      const legalities = JSON.parse(sanitizedParams.legalities);
-      Object.keys(legalities).forEach(key => {
-        if (legalities[key] === "legal") {
+    if (sanitizedLegalities) {
+      Object.keys(sanitizedLegalities).forEach(key => {
+        if (sanitizedLegalities[key] === "legal") {
           filter[`legalities.${key}`] = "legal";
         }
       });
